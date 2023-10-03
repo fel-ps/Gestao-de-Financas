78b38498bb4c7041bd469bc444584640e95791f4
Felipe Augusto Araújo da Cunha <araujofelipe54@gmail.com>
2023-10-02 23:50:29 -0300

    Update funcoes.h
    implementado assinaturas das funções

--- funcoes.h
+++ funcoes.h
@@ -1,3 +1,14 @@
 // ASSINATURA DAS FUNÇÕES
 
 void invalido(void);
+
+struct tm obterDataAtual();
+
+char verificarcpf(char *cpf, int tam);
+void ler_cpf(char *cpf);
+
+void ler_nome(char *name);
+int validar_letras(char *nome, int tam);
+
+void ler_telefone(char *telefone);
+int validaTelefone(char *telefone);
